7d1782d2152da7cc7dc41e25744ed89785e4ab45
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-22 01:52:35 -0500

    Add more ConnectionPool config values.

--- src/main/java/com/github/danielm94/config/ConnectionPoolConfiguration.java
+++ src/main/java/com/github/danielm94/config/ConnectionPoolConfiguration.java
@@ -1,10 +1,30 @@
 package com.github.danielm94.config;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public interface ConnectionPoolConfiguration {
 
-    int getMaximumPoolSize();
+    int getInitialMaxPoolSize();
     int getInitialPoolSize();
-    Duration getConnectionTimeout();
+    Duration getConnectionFromPoolTimeout();
+
+    Duration getConnectionLeakThreshold();
+
+    int getConnectionValidationTimeoutSeconds();
+
+    long getConnectionLeakDetectorServiceInterval();
+    TimeUnit getConnectionLeakDetectorServiceIntervalUnit();
+
+    double getHighLoadThreshold();
+
+    double getLowLoadThreshold();
+
+    int getMaximumPoolSize();
+
+    double getHighLoadPoolGrowthFactor();
+
+    double getLowLoadPoolShrinkFactor();
+
+    int getLowLoadHysteresisCount();
 }
--- src/main/java/com/github/danielm94/config/DefaultPoolConfiguration.java
+++ src/main/java/com/github/danielm94/config/DefaultPoolConfiguration.java
@@ -2,18 +2,29 @@ package com.github.danielm94.config;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
 
 public class DefaultPoolConfiguration implements ConnectionPoolConfiguration {
-    public static final int DEFAULT_MAXIMUM_POOL_SIZE = 50;
+    public static final int INITIAL_MAX_POOL_SIZE = 50;
 
     public static final int INITIAL_POOL_SIZE = 10;
 
-    public static final int CONNECTION_VALIDATION = 5;
-    public static final Duration DEFAULT_CONNECTION_TIMEOUT = Duration.of(10, ChronoUnit.MINUTES);
+    public static final int CONNECTION_VALIDATION_TIMEOUT_SECONDS = 10;
+    public static final Duration GET_CONNECTION_TIMEOUT = Duration.of(1, ChronoUnit.MINUTES);
+
+    public static final Duration CONNECTION_LEAK_THRESHOLD = Duration.of(5, ChronoUnit.MINUTES);
+    public static final int CONNECTION_LEAK_DETECTOR_SERVICE_INTERVAL = 2;
+    public static final TimeUnit CONNECTION_LEAK_DETECTOR_SERVICE_INTERVAL_UNIT = TimeUnit.MINUTES;
+    public static final double HIGH_LOAD_THRESHOLD = 0.9;
+    public static final int MAXIMUM_POOL_SIZE = 500;
+    public static final double HIGH_LOAD_GROWTH_FACTOR = 1.5;
+    public static final double LOW_LOAD_GROWTH_FACTOR = 0.1;
+    public static final int LOW_LOAD_HYSTERESIS_COUNT = 5;
+
 
     @Override
-    public int getMaximumPoolSize() {
-        return DEFAULT_MAXIMUM_POOL_SIZE;
+    public int getInitialMaxPoolSize() {
+        return INITIAL_MAX_POOL_SIZE;
     }
 
     @Override
@@ -22,7 +33,57 @@ public class DefaultPoolConfiguration implements ConnectionPoolConfiguration {
     }
 
     @Override
-    public Duration getConnectionTimeout() {
-        return DEFAULT_CONNECTION_TIMEOUT;
+    public Duration getConnectionFromPoolTimeout() {
+        return GET_CONNECTION_TIMEOUT;
+    }
+
+    @Override
+    public Duration getConnectionLeakThreshold() {
+        return CONNECTION_LEAK_THRESHOLD;
+    }
+
+    @Override
+    public int getConnectionValidationTimeoutSeconds() {
+        return CONNECTION_VALIDATION_TIMEOUT_SECONDS;
+    }
+
+    @Override
+    public long getConnectionLeakDetectorServiceInterval() {
+        return CONNECTION_LEAK_DETECTOR_SERVICE_INTERVAL;
+    }
+
+    @Override
+    public TimeUnit getConnectionLeakDetectorServiceIntervalUnit() {
+        return CONNECTION_LEAK_DETECTOR_SERVICE_INTERVAL_UNIT;
+    }
+
+    @Override
+    public double getHighLoadThreshold() {
+        return HIGH_LOAD_THRESHOLD;
+    }
+
+    @Override
+    public double getLowLoadThreshold() {
+        return 0.15;
+    }
+
+    @Override
+    public int getMaximumPoolSize() {
+        return MAXIMUM_POOL_SIZE;
+    }
+
+    @Override
+    public double getHighLoadPoolGrowthFactor() {
+        return HIGH_LOAD_GROWTH_FACTOR;
+    }
+
+    @Override
+    public double getLowLoadPoolShrinkFactor() {
+        return LOW_LOAD_GROWTH_FACTOR;
+    }
+
+    @Override
+    public int getLowLoadHysteresisCount() {
+        return LOW_LOAD_HYSTERESIS_COUNT;
     }
 }
