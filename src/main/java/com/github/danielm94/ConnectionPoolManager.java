df41d5e165565def923b9f09c310d22d5bd6b8f0
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-22 16:27:29 -0500

    Initial commit of ConnectionPoolManager

---
+++ src/main/java/com/github/danielm94/ConnectionPoolManager.java
@@ -0,0 +1,285 @@
+package com.github.danielm94;
+
+import com.github.danielm94.config.ConnectionPoolConfiguration;
+import com.github.danielm94.config.DefaultPoolConfiguration;
+import com.github.danielm94.credentials.ConnectionCredentials;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.extern.flogger.Flogger;
+import lombok.val;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Set;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Flogger
+public class ConnectionPoolManager {
+    private static volatile ConnectionPoolManager instance;
+    private final AtomicInteger poolCapacity;
+    private final AtomicInteger activeConnections = new AtomicInteger(0);
+    private final AtomicInteger lowLoadCount = new AtomicInteger(0);
+    private final ConnectionCredentials credentials;
+    private BlockingQueue<Connection> connectionPool;
+    private final ScheduledExecutorService leakDetectionService;
+    private final ConnectionLeakDetector leakDetector;
+    private final Set<Connection> activeConnectionSet;
+    private boolean leakDetectionScheduled = false;
+    @Setter
+    @Getter
+    private ConnectionPoolConfiguration config;
+
+
+    /**
+     * Private constructor for ConnectionPoolManager.
+     * Initializes the connection pool and other configurations.
+     *
+     * @param credentials The credentials used for creating database connections.
+     * @throws SQLException If a database access error occurs.
+     */
+    private ConnectionPoolManager(ConnectionCredentials credentials) throws SQLException {
+        this.config = new DefaultPoolConfiguration();
+        this.poolCapacity = new AtomicInteger(config.getInitialMaxPoolSize());
+        this.connectionPool = new ArrayBlockingQueue<>(poolCapacity.get());
+        this.credentials = credentials;
+        this.leakDetectionService = Executors.newSingleThreadScheduledExecutor();
+        this.leakDetector = new ConnectionLeakDetector(config.getConnectionLeakThreshold());
+        this.activeConnectionSet = ConcurrentHashMap.newKeySet();
+        fillConnectionPool(config.getInitialPoolSize());
+    }
+
+
+    /**
+     * Retrieves the singleton instance of ConnectionPoolManager.
+     * Throws an exception if the instance is not initialized yet.
+     *
+     * @return The singleton instance of ConnectionPoolManager.
+     * @throws IllegalStateException If the instance is not initialized.
+     */
+    public static synchronized ConnectionPoolManager getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("ConnectionPoolManager is not initialized. Call initialize() first.");
+        }
+        return instance;
+    }
+
+    /**
+     * Initializes the singleton instance of the ConnectionPoolManager.
+     * This method should be called once during the application startup.
+     *
+     * @param credentials The credentials used for creating database connections.
+     * @throws SQLException If a database access error occurs.
+     */
+    public static synchronized void initialize(ConnectionCredentials credentials) throws SQLException {
+        if (instance == null) {
+            instance = new ConnectionPoolManager(credentials);
+        }
+    }
+
+    /**
+     * Acquires a connection from the pool. If the pool is under high load, it attempts to expand the pool.
+     * Validates and potentially refreshes the connection before returning it.
+     *
+     * @return A valid database connection.
+     * @throws InterruptedException If the thread is interrupted while waiting for a connection.
+     * @throws SQLException If a database access error occurs or the connection is not valid.
+     */
+    public Connection getConnection() throws InterruptedException, SQLException {
+        val highLoadThreshold = config.getHighLoadThreshold();
+        val poolCapacityValue = poolCapacity.get();
+        if (isPoolUnderHighLoad(poolCapacityValue, highLoadThreshold)) {
+            handleHighLoad();
+            val numberOfNewConnections = (int) (poolCapacity.get() * config.getHighLoadConnectionGrowthFactor());
+            fillConnectionPool(numberOfNewConnections);
+        }
+        val timeout = config.getConnectionFromPoolTimeout();
+        var connection = connectionPool.poll(timeout.toMillis(), TimeUnit.MILLISECONDS);
+        if (connection == null) return null;
+        connection = validateConnection(connection);
+        leakDetector.registerConnection(connection);
+        scheduleLeakDetection();
+        activeConnections.incrementAndGet();
+        activeConnectionSet.add(connection);
+        return connection;
+    }
+
+    /**
+     * Returns a connection to the pool and checks for low load to potentially shrink the pool.
+     * Validates the connection before returning it to the pool.
+     *
+     * @param connection The database connection to return.
+     * @throws SQLException If a database access error occurs.
+     * @throws InterruptedException If the thread is interrupted while handling the connection return.
+     */
+    public void returnConnection(@NonNull Connection connection) throws SQLException, InterruptedException {
+        activeConnectionSet.remove(connection);
+        connection = validateConnection(connection);
+
+        if (!connectionPool.offer(connection)) {
+            log.atFine().log("Connection pool is full. Closing returned connection.");
+            connection.close();
+        }
+        activeConnections.decrementAndGet();
+        handleLowLoad();
+    }
+
+    /**
+     * Handles connections that are detected as leaky by closing and removing them from the active set.
+     *
+     * @param connection The leaky connection to handle.
+     * @throws SQLException If a database access error occurs during connection closure.
+     */
+    public void handleLeakyConnections(@NonNull Connection connection) throws SQLException {
+        activeConnectionSet.remove(connection);
+        connection.close();
+        activeConnections.decrementAndGet();
+
+    }
+
+    /**
+     * Handles low load scenarios by potentially shrinking the pool.
+     *
+     * @throws SQLException If a database access error occurs during pool resizing.
+     * @throws InterruptedException If the thread is interrupted while shrinking the pool.
+     */
+    private synchronized void handleLowLoad() throws SQLException, InterruptedException {
+        val poolCapacityValue = poolCapacity.get();
+        val minimumPossiblePoolSize = config.getInitialMaxPoolSize();
+        if (poolCapacityValue == minimumPossiblePoolSize) return;
+        val activeConnectionsValue = activeConnections.get();
+        val loadFactor = (double) activeConnectionsValue / poolCapacityValue;
+        val lowLoadThreshold = config.getLowLoadThreshold();
+
+        if (loadFactor < lowLoadThreshold) {
+            if (lowLoadCount.incrementAndGet() >= config.getLowLoadHysteresisCount()) {
+                val poolShrinkFactor = config.getLowLoadPoolShrinkFactor();
+
+                val newPoolSize = Math.max(minimumPossiblePoolSize, (int) (poolCapacityValue * poolShrinkFactor));
+                shrinkPool(newPoolSize);
+                lowLoadCount.set(0);
+            }
+        } else lowLoadCount.set(0);
+    }
+
+    /**
+     * Shrinks the connection pool to a new, smaller capacity.
+     * Closes connections that are removed from the pool.
+     *
+     * @param newPoolCapacity The new capacity for the pool.
+     * @throws InterruptedException If the thread is interrupted while shrinking the pool.
+     * @throws SQLException If a database access error occurs during connection closure.
+     */
+    private void shrinkPool(int newPoolCapacity) throws InterruptedException, SQLException {
+        poolCapacity.set(newPoolCapacity);
+        val newPool = new ArrayBlockingQueue<Connection>(newPoolCapacity);
+        while (connectionPool.size() > newPoolCapacity) {
+            val connection = connectionPool.take();
+            if (!activeConnectionSet.contains(connection)) {
+                connection.close();
+            } else {
+                newPool.offer(connection);
+            }
+        }
+        connectionPool.drainTo(newPool);
+        connectionPool = newPool;
+        log.atInfo().log("Connection pool resized. New capacity: %s", newPoolCapacity);
+    }
+
+    /**
+     * Handles high load scenarios by potentially expanding the pool.
+     */
+    private synchronized void handleHighLoad() {
+        val highLoadThreshold = config.getHighLoadThreshold();
+        val poolCapacityValue = poolCapacity.get();
+        final var poolUnderHighLoad = isPoolUnderHighLoad(poolCapacityValue, highLoadThreshold);
+        if (!poolUnderHighLoad) return;
+        val maximumPoolSize = config.getMaximumPoolSize();
+        if (poolCapacityValue == maximumPoolSize) return;
+
+        val poolGrowthFactor = config.getHighLoadPoolGrowthFactor();
+        val newPoolCapacity = Math.min((int) (poolCapacityValue * poolGrowthFactor), maximumPoolSize);
+
+        if (newPoolCapacity > poolCapacityValue) {
+            growPool(newPoolCapacity);
+        }
+    }
+
+
+    /**
+     * Checks if the pool is under high load based on the current capacity and load threshold.
+     *
+     * @param poolCapacityValue The current capacity of the pool.
+     * @param highLoadThreshold The threshold to determine high load.
+     * @return True if the pool is under high load, false otherwise.
+     */
+    private boolean isPoolUnderHighLoad(int poolCapacityValue, double highLoadThreshold) {
+        return (double) activeConnections.get() / poolCapacityValue > highLoadThreshold;
+    }
+
+    /**
+     * Expands the connection pool to a new, larger capacity.
+     *
+     * @param newPoolCapacity The new capacity for the pool.
+     */
+    private void growPool(int newPoolCapacity) {
+        poolCapacity.set(newPoolCapacity);
+        val newPool = new ArrayBlockingQueue<Connection>(newPoolCapacity);
+        connectionPool.drainTo(newPool);
+        connectionPool = newPool;
+        log.atInfo().log("Connection pool resized. New capacity: %s", newPoolCapacity);
+    }
+
+    /**
+     * Schedules the connection leak detection task if it's not already scheduled.
+     */
+    private void scheduleLeakDetection() {
+        if (leakDetectionScheduled) return;
+        val interval = config.getConnectionLeakDetectorServiceInterval();
+        val intervalUnit = config.getConnectionLeakDetectorServiceIntervalUnit();
+        leakDetectionService.schedule(leakDetector, interval, intervalUnit);
+        leakDetectionScheduled = true;
+    }
+
+    /**
+     * Fills the connection pool with a specified number of new connections.
+     *
+     * @param numberOfConnections The number of new connections to add to the pool.
+     * @throws SQLException If a database access error occurs while creating new connections.
+     */
+    private void fillConnectionPool(int numberOfConnections) throws SQLException {
+        log.atFinest().log("Adding %d connection(s) to the pool...", numberOfConnections);
+        for (var i = 0; i < numberOfConnections && connectionPool.size() < poolCapacity.get(); i++) {
+            connectionPool.add(createNewConnection());
+        }
+    }
+
+    /**
+     * Creates a new database connection using the provided credentials.
+     *
+     * @return A new database connection.
+     * @throws SQLException If a database access error occurs.
+     */
+    private Connection createNewConnection() throws SQLException {
+        return DriverManager.getConnection(credentials.getBaseDatabaseUrl(), credentials.getUsername(), credentials.getPassword());
+    }
+
+    /**
+     * Validates a database connection. Closes and replaces the connection if it is not valid.
+     *
+     * @param connection The database connection to validate.
+     * @return The validated (or new replacement) connection.
+     * @throws SQLException If a database access error occurs.
+     */
+    private Connection validateConnection(Connection connection) throws SQLException {
+        if (!connection.isValid(config.getConnectionValidationTimeoutSeconds())) {
+            log.atFine().log("Connection is no longer valid. Closing...");
+            leakDetector.deregisterConnection(connection);
+            connection.close();
+            connection = createNewConnection();
+        }
+        return connection;
+    }
+}
