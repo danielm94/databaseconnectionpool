f52a54f958449b74a866f69392126981960b7dc1
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-22 16:28:03 -0500

    Initial commit of MissingPropertyException

---
+++ src/main/java/com/github/danielm94/credentials/MissingPropertyException.java
@@ -0,0 +1,7 @@
+package com.github.danielm94.credentials;
+
+public class MissingPropertyException extends RuntimeException {
+    public MissingPropertyException(String message) {
+        super(message);
+    }
+}
