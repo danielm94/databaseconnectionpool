712d5968c54542413733a4fc5fa1b4a841f133a3
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-21 21:51:56 -0500

    Add lombok and flogger to build.gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,15 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     implementation("com.mysql:mysql-connector-j:8.2.0")
+
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    testCompileOnly ("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.30")
+
+    implementation("com.google.flogger:flogger-system-backend:0.8")
+    implementation("com.google.flogger:flogger:0.8")
 }
 
 tasks.test {
