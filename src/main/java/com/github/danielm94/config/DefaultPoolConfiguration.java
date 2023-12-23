5da6a959fcce6397152ea79423751841ba17624a
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-22 19:07:33 -0500

    Reorganize fields.

--- src/main/java/com/github/danielm94/config/DefaultPoolConfiguration.java
+++ src/main/java/com/github/danielm94/config/DefaultPoolConfiguration.java
@@ -7,20 +7,19 @@ import java.util.concurrent.TimeUnit;
 public class DefaultPoolConfiguration implements ConnectionPoolConfiguration {
     public static final int INITIAL_MAX_POOL_SIZE = 50;
     public static final int INITIAL_POOL_SIZE = 10;
-    public static final int CONNECTION_VALIDATION_TIMEOUT_SECONDS = 10;
     public static final Duration CONNECTION_TIMEOUT = Duration.of(5, ChronoUnit.SECONDS);
     public static final Duration CONNECTION_LEAK_THRESHOLD = Duration.of(5, ChronoUnit.MINUTES);
+    public static final int CONNECTION_VALIDATION_TIMEOUT_SECONDS = 10;
     public static final int CONNECTION_LEAK_DETECTOR_SERVICE_INTERVAL = 2;
     public static final TimeUnit CONNECTION_LEAK_DETECTOR_SERVICE_INTERVAL_UNIT = TimeUnit.MINUTES;
     public static final double HIGH_LOAD_THRESHOLD = 0.9;
+    public static final double LOW_LOAD_THRESHOLD = 0.15;
     public static final int MAXIMUM_POOL_SIZE = 125;
     public static final double HIGH_LOAD_GROWTH_FACTOR = 1.5;
-    public static final int LOW_LOAD_HYSTERESIS_COUNT = 5;
     public static final double HIGH_LOAD_CONNECTION_GROWTH_FACTOR = 0.1;
-    public static final double LOW_LOAD_POOL_SHRINK_FACTOR = 0.5;
     public static final int MAXIMUM_CONNECTION_GROWTH_AMOUNT = 8;
-    public static final double LOW_LOAD_THRESHOLD = 0.15;
-
+    public static final double LOW_LOAD_POOL_SHRINK_FACTOR = 0.5;
+    public static final int LOW_LOAD_HYSTERESIS_COUNT = 5;
 
     @Override
     public int getInitialMaxPoolSize() {
