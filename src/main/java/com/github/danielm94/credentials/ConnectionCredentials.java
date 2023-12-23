741dd44ac8327434a3fef8fa149e3d864b9d2dc7
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-23 00:04:30 -0500

    Add javadocs for ConnectionCredentials interface.

--- src/main/java/com/github/danielm94/credentials/ConnectionCredentials.java
+++ src/main/java/com/github/danielm94/credentials/ConnectionCredentials.java
@@ -1,7 +1,30 @@
 package com.github.danielm94.credentials;
 
+/**
+ * Interface defining the essential credentials required for establishing a database connection.
+ * Author: Daniel Martins
+ */
 public interface ConnectionCredentials {
+
+    /**
+     * Retrieves the username used for database authentication.
+     *
+     * @return The username for the database connection.
+     */
     String getUsername();
+
+    /**
+     * Retrieves the password used for database authentication.
+     *
+     * @return The password for the database connection.
+     */
     String getPassword();
+
+    /**
+     * Retrieves the base URL for the database. This URL typically includes the database type,
+     * host address, and may include additional parameters specific to the database.
+     *
+     * @return The base URL for connecting to the database.
+     */
     String getBaseDatabaseUrl();
 }
