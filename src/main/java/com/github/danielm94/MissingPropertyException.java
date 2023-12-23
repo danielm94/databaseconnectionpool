5b47bafa0dbf234ff21d8f1a8ce1d70b8ecff8bf
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-22 19:04:56 -0500

    Moved MissingPropertyException to root package

--- src/main/java/com/github/danielm94/MissingPropertyException.java
+++ src/main/java/com/github/danielm94/MissingPropertyException.java
@@ -1,4 +1,4 @@
-package com.github.danielm94.credentials;
+package com.github.danielm94;
 
 public class MissingPropertyException extends RuntimeException {
     public MissingPropertyException(String message) {
